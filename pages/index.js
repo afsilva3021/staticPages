7d28f4983691b5ae4a8ea3efb13dd4810d968afe
Alex Felix <afsilva3021@gmail.com>
2024-03-31 08:47:58 -0300

    atualização pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,33 @@
-import { useState} from 'react';
+import { useState } from 'react';
 
-function Home()
+
+function Home() 
 {
     return (
+
         <div>
             <h1>Home</h1>
-            <Contador />
-            <h5>Testando</h5>
         </div>
     )
 }
 
 
-function Contador()
-{
- const [contador,setContador] = useState(1);
 
-    function adicionarContador() 
-    {
-        setContador(contador+1)
-        console.log(contador);
-    }
+// function Contador() {
+//     const [contador, setContador] = useState(1);
 
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
+//     function adicionarContador() {
+//         setContador(contador + 1)
+//         console.log(contador);
+//     }
+
+//     return (
+//         <div>
+//             <div>{contador}</div>
+//             <button onClick={adicionarContador}>Adicionar</button>
+//         </div>
+//     )
+// }
 
 
 
