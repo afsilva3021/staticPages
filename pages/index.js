5a1e13174efd18f17e75e7d91bf3ad13c314ae67
Alex felix <afsilva3021@gmail.com>
2024-04-01 20:00:19 -0300

    atualização de layout

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,25 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import CollapsibleNavbar from "../Components/navbar";
+import React from 'react';
 
 function Home() {
-  
-  const image = require('../img/iphone-wallpaper.png');
+  const background = 'https://yesbrasil.com/wp-content/uploads/2023/12/Apple-iPhone-15-Pro-lineup-color-lineup-geo-230912_big.jpg.large_2x.jpg';
+
 
   return [
     <div>
       <CollapsibleNavbar />
     </div>,
+    <div>
     <Container className="container-lx d-flex justify-content-center mt-5">
-      <h1 className="d-flex justify-content-center">Home</h1>
       <Row>
         <Col>
-        <img src={image} />
+        <img src={background} style={{width: 900, height: 600}} />
         </Col>
       </Row>
     </Container>,
+    </div>,
   ];
 }
 
